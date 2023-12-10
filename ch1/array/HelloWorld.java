8d8a4b00f51801bf0e6c360f36b86dd0d2cf92cd
ZERA <z723622533@gmail.com>
2023-12-10 17:37:56 +0800

    ASUS EDITION

--- ch1/array/HelloWorld.java
+++ ch1/array/HelloWorld.java
@@ -2,6 +2,9 @@ package ch1.array;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+
+        System.out.println("HAHAH this is from asus");
     }
 }
