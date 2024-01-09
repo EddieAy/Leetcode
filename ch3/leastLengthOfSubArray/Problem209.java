bbe91e45934784e5d9441b5b2fa5ca82d48db4b0
EddieAy <z723622533@gmail.com>
2024-01-09 12:49:04 +0800

    2024 mid

--- ch3/leastLengthOfSubArray/Problem209.java
+++ ch3/leastLengthOfSubArray/Problem209.java
@@ -2,10 +2,10 @@ package ch3.leastLengthOfSubArray;
 
 public class Problem209 {
     public static void main(String[] args) {
-        System.out.println(minSubArrayLen(7,new int[] {2,3,1,2,4,3}));
+        System.out.println(minSubArrayLen1(7,new int[] {2,3,1,2,4,3}));
     }
 
-    public static int minSubArrayLen(int target, int[] nums) {
+    public static int minSubArrayLen1(int target, int[] nums) {
         int sum;
         int len;
         int minLen = (int)Math.pow(10,5) + 1;
@@ -31,5 +31,23 @@ public class Problem209 {
 
         }
         return minLen;
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        int i = 0;
+        int j = 0;
+        int len = 0;
+        int result = Integer.MAX_VALUE;
+        int sum = 0;
+        for (; j < nums.length; j++) {
+            sum+=nums[j];
+            while (sum >= target){
+                len = j - i + 1;
+                result = result <= len ? result:len;
+                sum-=nums[i++];
+            }
+        }
+
+        return result == Integer.MAX_VALUE?0:result;
     }
 }
