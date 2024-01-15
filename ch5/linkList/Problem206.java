4ab9af797697609bccc29a5dd5f4ccf29ba70f2e
ZERA <z723622533@gmail.com>
2024-01-15 23:54:56 +0800

    1.15 持续学习

---
+++ ch5/linkList/Problem206.java
@@ -0,0 +1,59 @@
+package ch5.linkList;
+
+public class Problem206 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // 打印原始链表
+        printList(head);
+
+        // 反转链表
+        head = reverseList(head);
+
+        // 打印反转后的链表
+        printList(head);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null){
+            return null;
+        }
+
+        ListNode currentNode = head;
+        ListNode preNode = null;
+        while (currentNode != null){
+            ListNode temp = currentNode.next;
+            currentNode.next = preNode;
+            preNode = currentNode;
+            currentNode = temp;
+        }
+
+        return preNode;
+    }
+
+    public static ListNode reverseList2(ListNode head) {
+        // 边缘条件判断
+        if (head == null) return null;
+        if (head.next == null) return head;
+        // 递归调用，翻转第二个节点开始往后的链表
+        ListNode last = reverseList2(head.next);
+        // 翻转头节点与第二个节点的指向
+        head.next.next = head;//head.next 指的是 4的next 5 的next  指向了 4
+        // 此时的 head 节点为尾节点，next 需要指向 NULL
+        head.next = null;      //4的next 指向了Null
+        return last;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
--- ch5/linkList/Problem707.java
+++ ch5/linkList/Problem707.java
@@ -1,4 +1,92 @@
 package ch5.linkList;
 
+//class ListNode{
+//    ListNode next;
+//    int val;
+//    public ListNode(){};
+//    public ListNode(int val){
+//        this.val = val;
+//    }
+//    public ListNode(int val, ListNode node){
+//        this.val = val;
+//        this.next = node;
+//    }
+//}
+
 public class Problem707 {
+
+    int size;
+    ListNode head;
+
+    public Problem707() {
+        size = 0;
+        this.head = new ListNode(0);
+    }
+
+    public int get(int index) {
+        if(index < 0 || index >= size){
+            return -1;
+        }
+        int i = 0;
+        ListNode node = head;
+        while(i != index + 1){
+            node = node.next;   // 1         
+            i++;
+        }
+        return node.val;
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0,val);
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size,val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0){
+            index = 0;
+        }
+        if(index > size){
+            return;
+        }
+        size++;
+        ListNode pre = head;
+        // 6    8
+        for(int i = 0;i < index;i++){
+            pre = pre.next;
+        }
+        ListNode newNode = new ListNode(val);
+        newNode.next = pre.next;
+        pre.next = newNode;
+
+    }
+
+    public void deleteAtIndex(int index) {
+        if(index < 0 || index >= size){
+            return;
+        }
+        size--;
+        if(index == 0){
+            head = head.next;
+            return;
+        }
+        ListNode pre = head;
+        for(int i = 0;i < index;i++){
+            pre = pre.next;
+        }
+        pre.next = pre.next.next;
+
+    }
 }
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
